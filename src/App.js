598a477454ba1ed1871cefc15b41c23aa3ef00e5
jinyoung <122714368+dev-edchoi@users.noreply.github.com>
2023-05-17 08:45:04 +0000

    about hooks

--- src/App.js
+++ src/App.js
@@ -2,13 +2,50 @@ import "./styles.css";
 import React, { useState } from "react";
 import ReactDom from "react-dom";
 
-export default function App() {
+const App = () => {
   const [item, setItem] = useState(0);
-
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={incrementItem}> increment </button>
+      <button onClick={decrementItem}> decrement </button>
     </div>
   );
+};
+
+class AppUgly extends React.Component {
+  state = {
+    item: 1,
+  };
+  render() {
+    const { item } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello {item}</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button onClick={this.incrementItem}> increment </button>
+        <button onClick={this.decrementItem}> decrement </button>
+      </div>
+    );
+  }
+  incrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1,
+      };
+    });
+  };
+
+  decrementItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1,
+      };
+    });
+  };
 }
+
+export default App;
